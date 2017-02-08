b0820f02437f00ec914b686efb2b07fe005979e1
Gregor Johannson <gregor134@hotmail.com>
2017-02-09 00:50:13 +0200

    Update readme.

--- service/src/main/java/com/rest_service/controller/ComputerController.java
+++ service/src/main/java/com/rest_service/controller/ComputerController.java
@@ -1,7 +1,7 @@
 package com.rest_service.controller;
 
-import com.rest_app.model.Computer;
-import com.rest_app.service.ComputerService;
+import com.rest_service.model.Computer;
+import com.rest_service.service.ComputerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
